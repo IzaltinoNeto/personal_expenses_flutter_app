bf9ff03d5f136e1dcbd59c3f606e640a242b45dc
Izaltino Viana <izaltino.viana@itbam.org.br>
2020-08-03 14:56:13 -0400

    add modal button and theming

--- lib/widgets/user_transactions.dart
+++ lib/widgets/user_transactions.dart
@@ -10,35 +10,10 @@ class UserTransactions extends StatefulWidget {
 }
 
 class _UserTransactionsState extends State<UserTransactions> {
-  final List<Transaction> _userTransactions = [
-    Transaction(
-        id: 't1', title: 'New Shoes', amount: 69.99, date: DateTime.now()),
-    Transaction(
-        id: 't1', title: 'New Shirt', amount: 30.99, date: DateTime.now()),
-  ];
-
-  void _addNewTransaction(String title, double amount) {
-    final newTx = Transaction(
-      title: title,
-      amount: amount,
-      date: DateTime.now(),
-      id: DateTime.now().toString(),
-    );
-
-    setState(() {
-      _userTransactions.add(newTx);
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: <Widget>[
-        NewTransaction(_addNewTransaction),
-        TransactionList(
-          transactions: _userTransactions,
-        ),
-      ],
+      children: <Widget>[],
     );
   }
 }
